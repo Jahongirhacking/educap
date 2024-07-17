9708f381db6a20479ea5dc3d92424c596959f28c
Jahongirhacking <Jahongirhacking@gmail.com>
2024-07-17 16:21:06 +0500

    feat: add university logos

--- assets/logos/index.ts
+++ assets/logos/index.ts
@@ -1,3 +1,13 @@
 export { default as organizer1 } from "./innovation_logo.png";
 export { default as organizer2 } from "./tashkent_logo.png";
 export { default as organizer3 } from "./bazz_game_logo.png";
+
+// Universities
+export { default as tatuInha } from "./tatu_inha.png";
+export { default as webster } from "./webster.png";
+export { default as westminster } from "./westminster.png";
+export { default as tdiu } from "./tdiu.png";
+
+// Games
+export { default as dotaLogo } from "./dota_2_logo.png";
+export { default as csLogo } from "./cs_logo.png";
