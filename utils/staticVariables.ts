923fc2879322cec4df049d8f2cd92dc245686dc2
Jahongirhacking <Jahongirhacking@gmail.com>
2024-07-18 19:48:04 +0500

    del: remove anchors

--- utils/staticVariables.ts
+++ utils/staticVariables.ts
@@ -25,32 +25,9 @@ import {
 import ITeam from "@/types/ITeam";
 import ICarouselImage from "@/types/ICarouselImage";
 
-export const navbarAnchor = [
-  {
-    key: "target",
-    href: "#target",
-    title: "Цель",
-  },
-  {
-    key: "about",
-    href: "#about",
-    title: "О турнире",
-  },
-  {
-    key: "results",
-    href: "#results",
-    title: "Итоги",
-  },
-  {
-    key: "albom",
-    href: "#albom",
-    title: "Фотоальбом",
-  },
-];
-
 export const languageSelect: ILang[] = [
   { value: "ru", label: "Русский" },
-  { value: "uz", label: "Uzbek tili" },
+  { value: "uz", label: "O‘zbek tili" },
   { value: "en", label: "English" },
 ];
 
