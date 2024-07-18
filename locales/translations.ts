fe4367c71abf210a75947a496b9c1a4298261f44
Jahongirhacking <Jahongirhacking@gmail.com>
2024-07-18 19:46:55 +0500

    feat: add translations

---
+++ locales/translations.ts
@@ -0,0 +1,173 @@
+const translations = {
+  ru: {
+    Navbar: {
+      anchors: [
+        {
+          key: "target",
+          href: "#target",
+          title: "Цель",
+        },
+        {
+          key: "about",
+          href: "#about",
+          title: "О турнире",
+        },
+        {
+          key: "results",
+          href: "#results",
+          title: "Итоги",
+        },
+        {
+          key: "albom",
+          href: "#albom",
+          title: "Фотоальбом",
+        },
+      ],
+    },
+    Hero: {
+      location: "Ташкент 2024",
+      sponsor: "Турнир Министерства высшего образования, науки и инноваций",
+      date: "Май 10-23",
+    },
+    Statistics: {
+      students: "Студентов",
+      teams: "Команд",
+      universities: "Университетов",
+    },
+    Goals: {
+      title: "Цели турнира",
+      paragraph:
+        "Основная цель турнира была популяризация киберспорта и обучение студентов работе на инфосистемах. Организаторы научили студентов работать на инфосистемах и проводить масштабные турниры в будущем.",
+    },
+    Organizers: {
+      title: "Организаторы",
+      paragraphs: [
+        "Организаторы обучали студентов работе на киберспортивной платформе Faceit, помогали создавать команды и регистрироваться на чемпионаты, включая их собственный турнир.",
+        "На киберсцене было десять компьютеров, а трансляции на YouTube набрали более 4 000 просмотров. Победители получили денежные призы, медали и кубки.",
+        "Финал турнира прошел 23 мая в Министерстве Высшего Образования, Науки и Инноваций, с русскоязычными и узбекоязычными комментаторами.",
+      ],
+    },
+    PrizeFund: {
+      title: "Призовой фонд",
+    },
+    Winners: {
+      team: "Команда",
+      representatives: "Представители",
+    },
+  },
+  en: {
+    Navbar: {
+      anchors: [
+        {
+          key: "target",
+          href: "#target",
+          title: "Target",
+        },
+        {
+          key: "about",
+          href: "#about",
+          title: "About the tournament",
+        },
+        {
+          key: "results",
+          href: "#results",
+          title: "Results",
+        },
+        {
+          key: "albom",
+          href: "#albom",
+          title: "Photo album",
+        },
+      ],
+    },
+    Hero: {
+      location: "Tashkent 2024",
+      sponsor:
+        "Tournament of the Ministry of Higher Education, Science and Innovation",
+      date: "May 10-23",
+    },
+    Statistics: {
+      students: "Students",
+      teams: "Teams",
+      universities: "Universities",
+    },
+    Goals: {
+      title: "Tournament Goals",
+      paragraph:
+        "The main goal of the tournament was to popularize e-sports and train students to work with information systems. The organizers taught students to work on information systems and hold large-scale tournaments in the future.",
+    },
+    Organizers: {
+      title: "Organizers",
+      paragraphs: [
+        "The organizers taught students how to work on the Faceit esports platform, helped them create teams and register for championships, including their own tournament.",
+        "There were ten computers on the cyber stage, and the YouTube stream received over 4,000 views. The winners received cash prizes, medals and cups.",
+        "The final of the tournament was held on May 23 at the Ministry of Higher Education, Science and Innovation, with Russian-speaking and Uzbek-speaking commentators.",
+      ],
+    },
+    PrizeFund: {
+      title: "Prize fund",
+    },
+    Winners: {
+      team: "Team",
+      representatives: "Representatives",
+    },
+  },
+  uz: {
+    Navbar: {
+      anchors: [
+        {
+          key: "target",
+          href: "#target",
+          title: "Maqsad",
+        },
+        {
+          key: "about",
+          href: "#about",
+          title: "Turnir haqida",
+        },
+        {
+          key: "results",
+          href: "#results",
+          title: "Natijalar",
+        },
+        {
+          key: "albom",
+          href: "#albom",
+          title: "Foto albom",
+        },
+      ],
+    },
+    Hero: {
+      location: "Toshkent 2024",
+      sponsor: "Oliy ta’lim, fan va innovatsiyalar vazirligi turniri",
+      date: "May 10-23",
+    },
+    Statistics: {
+      students: "Talaba",
+      teams: "Jamoa",
+      universities: "Universitet",
+    },
+    Goals: {
+      title: "Turnir Maqsadi",
+      paragraph:
+        "Turnirning asosiy maqsadi kibersportni ommalashtirish va talabalarni axborot tizimlari bilan ishlashga o‘rgatish edi. Tashkilotchilar talabalarga axborot tizimlarida ishlash va kelgusida keng ko‘lamli turnirlar o‘tkazishni o‘rgatdi.",
+    },
+    Organizers: {
+      title: "Tashkilotchilar",
+      paragraphs: [
+        "Tashkilotchilar talabalarga Faceit esports platformasida ishlashni o‘rgatishdi, ularga jamoalar yaratish va chempionatlarga, jumladan, o‘z turnirlariga ro‘yxatdan o‘tishda yordam berishdi.",
+        "Kiber sahnada o'nta kompyuter bo'lsa ham YouTube translatsiyasida 4000 dan ortiq ko'rishga erishildi. G‘oliblarga pul mukofotlari, medallar va kuboklar topshirildi.",
+        "Turnir finali 23-may kuni Oliy ta’lim, fan va innovatsiyalar vazirligida rus va o‘zbek tilli sharhlovchilar ishtirokida bo‘lib o‘tdi.",
+      ],
+    },
+    PrizeFund: {
+      title: "Mukofot fondi",
+    },
+    Winners: {
+      team: "Jamoa",
+      representatives: "Vakil",
+    },
+  },
+};
+
+export default translations;
